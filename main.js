14f0c5aa7826a0e8aced8c1e21915d63b056cf28
maTe966 <101216123+maTe966v2@users.noreply.github.com>
2022-03-10 18:48:40 +0100

    call function, show result in console.log

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-function isInRange(n) {
+function numberInRange(n) {
     if (n >= 100 && n <= 200) {
-        return "Liczba znajduje się w przedziale";
+        console.log("Liczba znajduje się w przedziale");
     } else {
-        return "Liczba nie znajduje się w przedziale";
+        console.log("Liczba nie znajduje się w przedziale");
     }
 }
 
-// Napisz funkcję, która przyjmuje jako parametr liczbę n.
-// Dowolną liczbę należy przekazać do parametru podczas wywoływania funkcji.
-
-// Funkcja ma sprawdzać, czy liczba n znajduje się w przedziale 100-200.
-// Jeżeli tak, w konsoli ma wyświetlić się napis "Liczba znajduje się w przedziale".
-// Jeżeli nie, w konsoli pojawi się "Liczba nie znajduje się w przedziale".
-
-// Plik wgraj do zdalnego repozytorium i podeślij link do sprawdzenia.
+numberInRange(155);
+numberInRange(225);
